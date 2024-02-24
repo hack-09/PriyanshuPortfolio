553e70621fe637154b96f10b98f0234e5e2badcf
Priyanshu Kumar <priyanshukumar9780@gmail.com>
2024-02-24 19:59:30 +0530

    commit

--- src/components/ContactSec.js
+++ src/components/ContactSec.js
@@ -13,7 +13,7 @@ function ContactSec() {
     <div className="" style={gradientStyle}>
         <div className="d-flex justify-content-center p-4" >
             <a href="https://github.com/hack-09" className="p-2"><img src="./img/gith.png" class="img-thumbnail" alt="..." style={size}/></a>
-            <a href="https://www.linkedin.com/in/priyanshukumar9/" className="p-2"><img src="./img/linkedin.png" class="img-thumbnail" alt="..." style={size}/></a>
+            <a href="https://www.linkedin.com/in/priyanshukumar9/" className="p-2"><img src="./img/linkedIn.png" class="img-thumbnail" alt="..." style={size}/></a>
             <a href="https://www.instagram.com/priyanshu_kumar_rai_/" className="p-2"><img src="./img/insta.png" class="img-thumbnail" alt="..." style={size}/></a>
             <a href="mailto:priyanshukumar9780@gmail.com" className="p-2"><img src="./img/mail.png" class="img-thumbnail" alt="..." style={size}/></a>
             <a href="tel:+7986025569" className="p-2"><img src="./img/phone.png" class="img-thumbnail" alt="..." style={size}/></a>
